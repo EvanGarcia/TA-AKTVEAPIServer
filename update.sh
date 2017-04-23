df586da6ef941f0bb96cd9f5a9b40cc379f58cbc
Luke Hollenback <luke.hollenback@gmail.com>
2017-04-22 18:21:31 -0700

    Added update script to replace multi-line cron job on server.

---
+++ update.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# This script is intended to be used to update the API Server running on an
+# AKTVE server instance. It is likely called by a cron job, but can also be
+# called manually if necessary.
+
+# Stop the server
+systemctl stop aktveapisvr
+systemctl disable aktveapisvr
+
+# Update the server
+su -s /bin/sh gitadmin-ta-aktveapiserver -c 'cd /opt/TA-AKTVEAPIServer && git pull && cd -'
+rsync -zvh /opt/TA-AKTVEAPIServer/aktveapisvr.service /etc/systemd/system/aktveapisvr.service
+export GOPATH=/opt/go && export GOBIN=$GOPATH/bin && export PATH=$PATH:/usr/local/go/bin:$GOBIN && cd /opt/TA-AKTVEAPIServer && go build -o /opt/TA-AKTVEAPIServer/ta-aktveapiserver && cd -
+
+# Restart the server
+systemctl enable aktveapisvr
+systemctl start aktveapisvr
